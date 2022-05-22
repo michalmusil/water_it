8ddea72fb82651c8d27f1f15a38b7668cac709d9
Michal Musil <musilmichal16@gmail.com>
2022-05-23 00:44:46 +0200

    Bottom navigation implemented and combined with navigation drawer

--- app/src/main/java/cz/mendelu/xmusil5/waterit/activities/MainActivity.kt
+++ app/src/main/java/cz/mendelu/xmusil5/waterit/activities/MainActivity.kt
@@ -1,17 +1,12 @@
 package cz.mendelu.xmusil5.waterit.activities
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import cz.mendelu.xmusil5.waterit.R
 import cz.mendelu.xmusil5.waterit.databinding.ActivityMainBinding
@@ -21,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var navigationView: NavigationView
     private lateinit var appBarConfiguration: AppBarConfiguration
 
 
@@ -34,17 +31,32 @@ class MainActivity : AppCompatActivity() {
         // SETTING UP THE NAVIGATION
         navController = findNavController(R.id.nav_host_fragment_content_main)
         drawerLayout = binding.drawerLayout
+
+        navigationView = binding.navigationView
+        bottomNavigation = findViewById(R.id.bottomNavigation)
+
         // only adding the top level fragments - these fragments reset the navigation nesting and display burger icon upon entering them
         appBarConfiguration = AppBarConfiguration(setOf(
             R.id.PlantsListFragment,
             R.id.RoomsListFragment
         ), drawerLayout)
 
-        val navigationView: NavigationView = binding.navigationView
+        bottomNavigation.setupWithNavController(navController)
         navigationView.setupWithNavController(navController)
-        // END OF SETTING UP THE NAVIGATION
 
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+        // When an item in Navigation view or bottom navigation is selected, backstack is popped
+        bottomNavigation.setOnItemSelectedListener {
+            NavigationUI.onNavDestinationSelected(it, navController)
+            navController.popBackStack(it.itemId, false)
+        }
+        navigationView.setNavigationItemSelectedListener {
+            drawerLayout.closeDrawers()
+            NavigationUI.onNavDestinationSelected(it, navController)
+            navController.popBackStack(it.itemId, false)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
